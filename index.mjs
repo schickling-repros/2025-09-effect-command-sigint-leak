66caa22281189d5adaf6efcfdf5ab8ae507e436d
Johannes Schickling <schickling.j@gmail.com>
2025-10-01 12:02:20 +0200

    Ensure local node_modules and provide layers

--- index.mjs
+++ index.mjs
@@ -1,9 +1,14 @@
 import { dirname } from "node:path"
 import { fileURLToPath } from "node:url"
 
-import { Effect } from "effect"
+import { Effect, Layer } from "effect"
 import * as Command from "@effect/platform/Command"
-import { NodeCommandExecutor, NodeRuntime } from "@effect/platform-node"
+import {
+  NodeCommandExecutor,
+  NodeContext,
+  NodeFileSystem,
+  NodeRuntime,
+} from "@effect/platform-node"
 
 const cwd = dirname(fileURLToPath(import.meta.url))
 
@@ -15,6 +20,6 @@ const command = Command.make(process.execPath, ["child.mjs"]).pipe(
   Command.exitCode,
 )
 
-const program = Effect.provide(command, NodeCommandExecutor.layer)
+const layer = Layer.mergeAll(NodeContext.layer, NodeFileSystem.layer, NodeCommandExecutor.layer)
 
-NodeRuntime.runMain(program)
+NodeRuntime.runMain(command.pipe(Effect.provide(layer)))
