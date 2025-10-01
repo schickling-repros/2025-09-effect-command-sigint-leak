89f132da501095b7ec9b6541de671aa1b09ae3c2
Johannes Schickling <schickling.j@gmail.com>
2025-10-01 14:03:29 +0200

    Run child via bash shell

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "node:path"
 import { fileURLToPath } from "node:url"
 
-import { Effect } from "effect"
+import { Effect, Layer } from "effect"
 import * as Command from "@effect/platform/Command"
 import {
   NodeCommandExecutor,
@@ -11,19 +11,17 @@ import {
 } from "@effect/platform-node"
 
 const cwd = dirname(fileURLToPath(import.meta.url))
+const envPort = process.env.REPRO_PORT ?? "48787"
 
-const command = Command.make(process.execPath, ["child.mjs"]).pipe(
+const command = Command.make("bash", "-lc", "node child.mjs").pipe(
   Command.workingDirectory(cwd),
   Command.stdin("inherit"),
   Command.stdout("inherit"),
   Command.stderr("inherit"),
+  Command.env([["REPRO_PORT", envPort]]),
   Command.exitCode,
 )
 
-const program = command.pipe(
-  Effect.provide(NodeCommandExecutor.layer),
-  Effect.provide(NodeFileSystem.layer),
-  Effect.provide(NodeContext.layer),
-)
+const layer = Layer.mergeAll(NodeContext.layer, NodeFileSystem.layer, NodeCommandExecutor.layer)
 
-NodeRuntime.runMain(program)
+NodeRuntime.runMain(command.pipe(Effect.provide(layer)))
